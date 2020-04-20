b62cb265a8aeb4d8edef7655d50d1f9019e3f2d8
Jorge Vera <me@jorgevera.net>
2020-04-19 20:25:00 -0400

    Add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -7,5 +7,9 @@ module.exports = {
         content: ["./public/index.html"],
         defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       }),
+    process.env.NODE_ENV === "production" &&
+      require("cssnano")({
+        preset: "default",
+      }),
   ],
 };
